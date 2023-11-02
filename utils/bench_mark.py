bd115e3f93a3fc5ea4e079aa1c3fa58a80b40d9b
Bastien Hoorelbeke <44336050+Bast-94@users.noreply.github.com>
2023-11-02 16:17:48 +0000

    FIX(data)

--- utils/bench_mark.py
+++ utils/bench_mark.py
@@ -20,7 +20,7 @@ def save_bench_mark(bench_mark_df):
     formatted_df = bench_mark_df.round(3)
     # size and negative should integer columns
     formatted_df[['size', 'negative']] = formatted_df[['size', 'negative']].astype(int)
-    column_mapping = {'size': 'Dimension', 'negative': 'Negative samples', 'mean_rank': 'Mean Rank', 'map': 'MAP', 'spearman': 'Spearman coefficient'}
+    column_mapping = {'size': 'Dimension', 'negative': 'Negative samples', 'mean_rank': 'Mean Rank', 'map': 'MAP'}#, 'spearman': 'Spearman coefficient'}
     formatted_df = formatted_df.rename(columns=column_mapping)
     
     fig = plt.figure()
@@ -35,10 +35,10 @@ def save_bench_mark(bench_mark_df):
     
 def generate_bench_mark(train_relation_file_path,test_relation_file_path):
     relations = PoincareRelations(train_relation_file_path)
-    
+    print(relations)
     size_set= [5,10,20,50, 100, 200]
     negative_set = [5, 10, 20]
-    epochs_set = [50]
+    epochs_set = [0]
     
     parameters_grid = list(product_dict(epochs=epochs_set, size=size_set, negative=negative_set))
     model_evaluations = []
@@ -52,10 +52,10 @@ def generate_bench_mark(train_relation_file_path,test_relation_file_path):
         model.train(epochs=epochs, print_every=1,batch_size=10)
         
         lp_evaluation = LinkPredictionEvaluation(train_relation_file_path,test_relation_file_path, model.kv)
-        lexical_evaluation = LexicalEntailmentEvaluation(train_relation_file_path)
-        spearman = lexical_evaluation.evaluate_spearman(model.kv)
+        #lexical_evaluation = LexicalEntailmentEvaluation(train_relation_file_path)
+        #spearman = lexical_evaluation.evaluate_spearman(model.kv)
         mean_rank, map_value = lp_evaluation.evaluate().values()
-        eval_result = dict(size=current_size, negative=negative, mean_rank=mean_rank, map=map_value,spearman=spearman)
+        eval_result = dict(size=current_size, negative=negative, mean_rank=mean_rank, map=map_value)#,spearman=spearman)
         model_evaluations.append(eval_result)
 
     bench_mark_df = pd.DataFrame(model_evaluations)
@@ -64,10 +64,12 @@ def generate_bench_mark(train_relation_file_path,test_relation_file_path):
 if __name__ == "__main__":
     
     
-    train_relations = os.path.join(DATA_DIRECTORY, 'train_relation.tsv')
-    test_relations = os.path.join(DATA_DIRECTORY, 'test_relation.tsv')
+    train_relations_path = os.path.join(DATA_DIRECTORY, 'train_relation.tsv')
+    test_relations_path = os.path.join(DATA_DIRECTORY, 'test_relation.tsv')
+    # open train and test relations
     
-    generate_bench_mark(train_relations,test_relations)
+    
+    generate_bench_mark(train_relations_path,test_relations_path)
     
     
     
